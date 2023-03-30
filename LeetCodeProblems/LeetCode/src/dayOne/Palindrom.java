e817fd9c3745393fe3027475bb0846932a48b4e2
Kytsync <111482114+Kytsync@users.noreply.github.com>
2023-03-30 14:43:07 -0500

    palindrom question pushed

--- LeetCodeProblems/LeetCode/src/dayOne/Palindrom.java
+++ LeetCodeProblems/LeetCode/src/dayOne/Palindrom.java
@@ -1,7 +1,18 @@
 package dayOne;
 
 public class Palindrom {
+    public static boolean isPalindrome(int x) {
+        String given = String.valueOf(x);
+        int last= given.length() -1;
+        for(int i=0; i< given.length(); i++  ) {
+            if(given.charAt(i) != (given.charAt(last))) {
+                return false;
+            }
+            last --;
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        System.out.println("This is something new ");
+        System.out.println(isPalindrome(121));
     }
 }
