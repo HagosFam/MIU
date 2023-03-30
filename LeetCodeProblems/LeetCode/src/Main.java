67c73c165119b3b827b2b4ce23bf5cb022121dec
Kytsync <111482114+Kytsync@users.noreply.github.com>
2023-03-30 14:37:15 -0500

    palindrom question solved

--- LeetCodeProblems/LeetCode/src/Main.java
+++ LeetCodeProblems/LeetCode/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+    public static boolean isPalindrome(int x) {
+        String given = String.valueOf(x);
+        int last= given.length() -1;
+        for(int i=0; i< given.length(); i++  ) {
+            if(given.charAt(i) != (given.charAt(last))) {
+                return false;
+            }
+            last --;
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome(121));
     }
 }
